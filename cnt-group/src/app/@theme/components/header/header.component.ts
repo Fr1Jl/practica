50eba705d00482bd62feda31c126bfe255e25dbb
Fr1Jl <daniil15082008@yandex.ru>
2026-02-01 21:59:03 +0300

    4

---
+++ cnt-group/src/app/@theme/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'app-header',
+    template: './header.component.html',
+    styleUrls: ['./heander.component.css']
+})
+export class HeaderComponent {
+
+    title: string = 'CNT Group';
+    
+}
