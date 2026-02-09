86f4945f5d4c7f48b87eef56b9ac4cfeae3de833
Fr1Jl <daniil15082008@yandex.ru>
2026-02-10 01:14:23 +0300

    10

--- cnt-group/src/app/pages/sections/modules/administration/pages/users-page/users-page.component.ts
+++ cnt-group/src/app/pages/sections/modules/administration/pages/users-page/users-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserRegister } from 'src/app/domains/users/models/user.model';
+import { UserService } from 'src/app/domains/users/services/user.service';
 
 @Component({
   selector: 'app-users-page',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./users-page.component.css']
 })
 export class UsersPageComponent {
-
+  users: UserRegister[] = this.userService.users;
+  constructor(private userService: UserService) {}
 }
