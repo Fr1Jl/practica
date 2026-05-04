5944759a48825232d3feca510116e82f2a4aeb4b
Fr1Jl <daniil15082008@yandex.ru>
2026-05-05 01:20:24 +0300

    14

---
+++ cnt-group/src/main.py
@@ -0,0 +1,8 @@
+DB_CONFIG = dict(
+   host="147.45.184.12",  # localhost
+   user="root",  # логин от базы
+   password="root",  # пароль от базы
+   database="pip",  # имя базы
+   charset="utf8",
+   autocommit=True,
+)
